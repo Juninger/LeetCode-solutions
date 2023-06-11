9765743662ea053ec6ef1ac76a04f5b0392e5de0
Juninger <marcus.juninger@gmail.com>
2023-06-11 23:39:37 +0200

    Create LC_242_ValidAnagram.java

---
+++ LC_242_ValidAnagram.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+public class LC_242_ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        //converts both strings to charArrays
+        char[] w1 = s.toCharArray();
+        char[] w2 = t.toCharArray();
+
+        //sorts both charArrays
+        Arrays.sort(w1);
+        Arrays.sort(w2);
+
+        //if arrays are equal return true, otherwise return false
+        return Arrays.equals(w1, w2);
+    }
+}
