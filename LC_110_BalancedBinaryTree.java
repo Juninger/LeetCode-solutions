4a111276ecd2c83dde43df3af716a8bc9185f1d7
Juninger <marcus.juninger@gmail.com>
2024-05-17 02:22:57 +0200

    Update LC_110_BalancedBinaryTree.java

--- LC_110_BalancedBinaryTree.java
+++ LC_110_BalancedBinaryTree.java
@@ -6,28 +6,55 @@ class LC_110_BalancedBinaryTree {
 
     // driver method for solution --> DFSbalance()
     public boolean isBalanced(TreeNode root) {
-        return DFSbalance(root).getKey();
+        return DFSbalance(root)[0] == 1; // [0] : 1/0 --> true/false
     }
 
     // DFS solution: recursively checks if subtrees are balanced and computes their height
-    public Pair<Boolean, Integer> DFSbalance(TreeNode root) {
+    // [0] --> balanced: 1/0 --> true/false
+    // [1] --> height
+    public int[] DFSbalance(TreeNode root) {
 
-        // base case (empty subtree, balanced with height 0)
-        if (root == null) return new Pair<>(true, 0);
+        // base case (empty subtree, balanced (1) with height 0)
+        if (root == null) return new int[]{1, 0};
 
-        // recursive call to check subtree balance and height (< balanced, height >)
-        Pair<Boolean, Integer> left = DFSbalance(root.left);
-        Pair<Boolean, Integer> right = DFSbalance(root.right);
+        // recursive call to check subtree balance and height --> int[]{balanced, height}
+        int[] left = DFSbalance(root.left);
+        int[] right = DFSbalance(root.right);
 
         // true if subtrees are balanced (not null & height diff <= 1)
-        boolean balanced = (left.getKey() && right.getKey())
-                && (Math.abs(left.getValue() - right.getValue()) <= 1);
+        boolean balanced = (left[0] == 1 && right[0] == 1)
+                && (Math.abs(left[1] - right[1]) <= 1);
 
         // max height of current subtrees in this recursion
-        int height = 1 + Math.max(left.getValue(), right.getValue());
+        int height = 1 + Math.max(left[1], right[1]);
 
-        return new Pair<Boolean, Integer>(balanced, height);
+        return new int[]{balanced ? 1 : 0, height};
     }
+
+//    // driver method for solution --> DFSbalance()
+//    public boolean isBalanced(TreeNode root) {
+//        return DFSbalance(root).getKey();
+//    }
+//
+//    // DFS solution: recursively checks if subtrees are balanced and computes their height
+//    public Pair<Boolean, Integer> DFSbalance(TreeNode root) {
+//
+//        // base case (empty subtree, balanced with height 0)
+//        if (root == null) return new Pair<>(true, 0);
+//
+//        // recursive call to check subtree balance and height (< balanced, height >)
+//        Pair<Boolean, Integer> left = DFSbalance(root.left);
+//        Pair<Boolean, Integer> right = DFSbalance(root.right);
+//
+//        // true if subtrees are balanced (not null & height diff <= 1)
+//        boolean balanced = (left.getKey() && right.getKey())
+//                && (Math.abs(left.getValue() - right.getValue()) <= 1);
+//
+//        // max height of current subtrees in this recursion
+//        int height = 1 + Math.max(left.getValue(), right.getValue());
+//
+//        return new Pair<Boolean, Integer>(balanced, height);
+//    }
 
     // definition for a binary tree node
     public static class TreeNode {
