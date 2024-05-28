b734803a4378fbb142e2f9786f2a4568ef8fdb44
Juninger <marcus.juninger@gmail.com>
2024-05-28 15:07:16 +0200

    Update NeetCodeCore.java

--- NeetCodeCore.java
+++ NeetCodeCore.java
@@ -1,8 +1,93 @@
+import java.util.ArrayList;
+
 /**
  * This class holds a collection of "core skill" problems
  * to solve from NeetCode.io (https://neetcode.io/practice).
  */
 public class NeetCodeCore {
+
+    // Design a singly linked list class
+    class DesignSinglyLinkedList {
+
+        // definition for a single node in the linked list
+        class ListNode {
+            private int value;
+            private ListNode next;
+
+            ListNode(int value, ListNode next) {
+                this.value = value;
+                this.next = next;
+            }
+        }
+
+        private ListNode head; // start of the list
+        private ListNode tail; // end of the llist
+
+        // will initialize an empty linked list
+        public DesignSinglyLinkedList() {
+            // dummy node, helps removal and insertions at start of list
+            head = new ListNode(-1, null);
+            tail = head;
+        }
+
+        // will return the value of the ith node (0-indexed). If the index is out of bounds, return -1
+        public int get(int index) {
+            ListNode current = head.next;
+            int i = 0;
+            // traverse list until we find node at index (or reach out of bounds)
+            while (current != null) {
+                if (i == index) return current.value; // node found
+                i++;
+                current = current.next;
+            }
+            return -1; // index out of bounds
+        }
+
+        // will insert a node with val at the head of the list
+        public void insertHead(int val) {
+            // adds new head and moves old node +1 index
+            ListNode newNode = new ListNode(val, head.next);
+            head.next = newNode; // moves dummy pointer
+            if (newNode.next == null) tail = newNode; // if list was empty before insertion, move tail pointer
+        }
+
+        // will insert a node with val at the tail of the list
+        public void insertTail(int val) {
+            tail.next = new ListNode(val, null); // adds new node
+            tail = tail.next; // moves tail pointer
+        }
+
+        // will remove the ith node (0-indexed). If the index is out of bounds, return false, otherwise return true
+        public boolean remove(int index) {
+            ListNode current = head;
+            int i = 0;
+            while (i < index && current != null) {
+                // move pointer to node BEFORE the target node
+                i++;
+                current = current.next;
+            }
+            // check to see if both the current AND the target node is not null
+            if (current != null && current.next != null) {
+                // if we delete the tail, update tail pointer
+                if (current.next == tail) tail = current;
+                // shift next pointer to "delete" target
+                current.next = current.next.next;
+                return true;
+            }
+            return false; // out of bounds
+        }
+
+        // return an array of all the values in the linked list, ordered from head to tail
+        public ArrayList<Integer> getValues() {
+            ListNode current = head.next;
+            ArrayList<Integer> values = new ArrayList<>();
+            while (current != null) {
+                values.add(current.value);
+                current = current.next;
+            }
+            return values;
+        }
+    }
 
     /**
      * Design a Dynamic Array (aka a resizable array) class, such as an ArrayList in Java or a vector in C++
