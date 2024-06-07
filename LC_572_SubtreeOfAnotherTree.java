850c6866f7d40ac10613799d98b233d82c4dcbc4
Juninger <marcus.juninger@gmail.com>
2024-06-07 17:31:39 +0200

    Update LC_572_SubtreeOfAnotherTree.java

--- LC_572_SubtreeOfAnotherTree.java
+++ LC_572_SubtreeOfAnotherTree.java
@@ -6,7 +6,28 @@
 public class LC_572_SubtreeOfAnotherTree {
 
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        return false;
+        if (subRoot == null) return true; // no subtree (considered a subtree of ANY tree in this case)
+        if (root == null) return false; // no main tree
+        if (sameTree(root, subRoot)) return true; // full trees are identical
+
+        // recursively check if subRoot is a subtree of the left or right subtrees of root
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    // helper method, partly borrowed from LC_100_SameTree
+    private boolean sameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) return true; // both trees are null : identical
+
+        // if trees are not null and values are the same, recursively check if subtrees are identical
+        if (root != null && subRoot != null && root.val == subRoot.val) {
+
+            // recursively check if left and right subtrees are identical
+            boolean left = sameTree(root.left, subRoot.left);
+            boolean right = sameTree(root.right, subRoot.right);
+
+            return left && right; // true if nodes are identical
+        }
+        return false; // trees are not identical
     }
 
     // definition for a binary tree node
