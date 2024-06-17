3318be36883026a702622a6781b5342a5a89f3c7
Juninger <marcus.juninger@gmail.com>
2024-06-17 13:56:51 +0200

    Update LC_70_ClimbingStairs.java

--- LC_70_ClimbingStairs.java
+++ LC_70_ClimbingStairs.java
@@ -5,10 +5,31 @@
  */
 public class LC_70_ClimbingStairs {
 
+    // dynamic programming solution with bottom-up approach
+    public int climbStairs(int n) {
+
+        // base cases
+        if (n == 1) return 1; // one way to climb 1 step
+        if (n == 2) return 2; // two ways to climb 2 steps (1+1 or 2)
+
+        // stores computed results for how many ways to reach i:th step
+        int[] results = new int[n + 1];
+        results[1] = 1; // 1 way to reach first step
+        results[2] = 2; // 2 ways to reach second step
+
+        // calculate ways to reach each step
+        for (int i = 3; i <= n; i++) {
+            results[i] = results[i - 1] + results[i - 2];
+        }
+
+        // ways to reach the final step is stored in the last value of array
+        return results[n];
+    }
+
     private int[] memo; // used to memoize results of subproblems
 
     // brute force solution but with memoization for improved efficiency
-    public int climbStairs(int n) {
+    public int climbStairs2(int n) {
 
         // initialize all values in memoization array to -1 to indicate non-computed values
         memo = new int[n + 1]; // +1 because we start from "step 0"
@@ -34,12 +55,12 @@ public class LC_70_ClimbingStairs {
 
     // brute force solution: calculate all permutations, inefficient and much redundant work
     // essentially creates a decision-tree structure with lots of overlapping calculations: O(2^n)
-    public int climbStairs2(int n) {
+    public int climbStairs3(int n) {
         // base cases for small stairs or end of recursion
         if (n == 1) return 1; // one way to climb 1 step
         if (n == 2) return 2; // two ways to climb 2 steps (1+1 or 2)
 
         // for (n > 2): the way to climb n steps depends on the ways to climb the previous two steps
-        return climbStairs2(n - 1) + climbStairs2(n - 2); // recursively sum subproblems
+        return climbStairs3(n - 1) + climbStairs3(n - 2); // recursively sum subproblems
     }
 }
