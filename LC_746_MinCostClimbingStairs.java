bd8bfc9cf6c039153f27df8b020e854b99268b95
Juninger <marcus.juninger@gmail.com>
2024-06-20 12:17:56 +0200

    Update LC_746_MinCostClimbingStairs.java

--- LC_746_MinCostClimbingStairs.java
+++ LC_746_MinCostClimbingStairs.java
@@ -11,18 +11,40 @@ import java.util.Arrays;
  */
 public class LC_746_MinCostClimbingStairs {
 
-    // brute force solution with memoization
+    // dynamic programming solution with bottom-up approach
     public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length; // also the "index" of the "step" we need to reach
+        if (n == 0) return 0; // no steps
+        if (n == 1) return cost[0]; // only one step
+
+        int[] results = new int[n]; // stores computed results for cost of reaching i:th step
+        results[0] = cost[0]; // cost for reaching first step
+        results[1] = cost[1]; // cost for reaching second step
+
+        // calculate costs for reaching each step in input array
+        for (int i = 2; i < n; i++) {
+            // cost of current step + the MIN cost of reaching the previous two steps
+            results[i] = cost[i] + Math.min(results[i - 1], results[i - 2]);
+        }
+
+        // minimum cost to reach the top is the minimum cost of reaching any of the last two steps
+        return Math.min(results[n - 1], results[n - 2]);
+    }
+
+    /* -------------------------------------------------------------------------------------------- */
+
+    // brute force solution with memoization
+    public int minCostClimbingStairs2(int[] cost) {
         int n = cost.length; // also the "index" of the "step" we need to reach
         int[] memo = new int[n]; // stores already calculated values for fast retrieval
         Arrays.fill(memo, -1); // initializes all values to -1 to indicate non-calculated steps
 
         // start recursion to calculate cost of reaching the top ("index" n)
-        return Math.min(minCost(cost, n - 1, memo), minCost(cost, n - 2, memo));
+        return Math.min(minCost2(cost, n - 1, memo), minCost2(cost, n - 2, memo));
     }
 
     // recursively calculate cost to reach i:th step
-    public int minCost(int[] cost, int index, int[] memo) {
+    public int minCost2(int[] cost, int index, int[] memo) {
         if (index == 0) return cost[0]; // cost of first step
         if (index == 1) return cost[1]; // cost of second step
 
@@ -30,7 +52,7 @@ public class LC_746_MinCostClimbingStairs {
         if (memo[index] != -1) return memo[index];
 
         // calculate and memoize cost of reaching current step (cost of current step + the MIN cost of reaching the previous two steps)
-        memo[index] = cost[index] + Math.min(minCost(cost, index - 1, memo), minCost(cost, index - 2, memo));
+        memo[index] = cost[index] + Math.min(minCost2(cost, index - 1, memo), minCost2(cost, index - 2, memo));
 
         return memo[index];
     }
@@ -38,19 +60,19 @@ public class LC_746_MinCostClimbingStairs {
     /* -------------------------------------------------------------------------------------------- */
 
     // brute force solution, highly inefficient in both space and time
-    public int minCostClimbingStairs2(int[] cost) {
+    public int minCostClimbingStairs3(int[] cost) {
         int n = cost.length; // also the "index" of the "step" we need to reach
 
         // start recursion to calculate cost of reaching the top ("index" n)
-        return Math.min(minCost2(cost, n - 1), minCost2(cost, n - 2));
+        return Math.min(minCost3(cost, n - 1), minCost3(cost, n - 2));
     }
 
     // recursively calculate cost to reach i:th step
-    public int minCost2(int[] cost, int index) {
+    public int minCost3(int[] cost, int index) {
         if (index == 0) return cost[0]; // cost of first step
         if (index == 1) return cost[1]; // cost of second step
 
         // cost for i:th step == cost of current step + the MIN cost of reaching the previous two steps
-        return cost[index] + Math.min(minCost2(cost, index - 1), minCost2(cost, index - 2));
+        return cost[index] + Math.min(minCost3(cost, index - 1), minCost3(cost, index - 2));
     }
 }
