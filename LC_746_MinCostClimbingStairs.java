f8519d7608b6a926b5ac11678e9094ab723de106
Juninger <marcus.juninger@gmail.com>
2024-06-21 13:43:38 +0200

    Update LC_746_MinCostClimbingStairs.java

--- LC_746_MinCostClimbingStairs.java
+++ LC_746_MinCostClimbingStairs.java
@@ -11,8 +11,31 @@ import java.util.Arrays;
  */
 public class LC_746_MinCostClimbingStairs {
 
-    // dynamic programming solution with bottom-up approach
+    // dynamic programming solution with constant space
     public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length; // also the "index" of the "step" we need to reach
+        if (n == 0) return 0; // no steps
+        if (n == 1) return cost[0]; // only one step
+
+        // tracks minimum cost to reach the two previous step from the current step
+        int twoStepsBack = cost[0];
+        int oneStepBack = cost[1];
+
+        // calculate cost to reach the top, starting from the third step
+        for (int i = 2; i < n; i++) {
+            int thisStep = cost[i] + Math.min(twoStepsBack, oneStepBack);
+            twoStepsBack = oneStepBack; // move pointer for twoStepsBack to oneStepBack
+            oneStepBack = thisStep; // move pointer for oneStepBack to current step
+        }
+
+        // The minimum cost to reach the top is the minimum of reaching the last two steps
+        return Math.min(twoStepsBack, oneStepBack);
+    }
+
+    /* -------------------------------------------------------------------------------------------- */
+
+    // dynamic programming solution with bottom-up approach
+    public int minCostClimbingStairs2(int[] cost) {
         int n = cost.length; // also the "index" of the "step" we need to reach
         if (n == 0) return 0; // no steps
         if (n == 1) return cost[0]; // only one step
@@ -34,17 +57,17 @@ public class LC_746_MinCostClimbingStairs {
     /* -------------------------------------------------------------------------------------------- */
 
     // brute force solution with memoization
-    public int minCostClimbingStairs2(int[] cost) {
+    public int minCostClimbingStairs3(int[] cost) {
         int n = cost.length; // also the "index" of the "step" we need to reach
         int[] memo = new int[n]; // stores already calculated values for fast retrieval
         Arrays.fill(memo, -1); // initializes all values to -1 to indicate non-calculated steps
 
         // start recursion to calculate cost of reaching the top ("index" n)
-        return Math.min(minCost2(cost, n - 1, memo), minCost2(cost, n - 2, memo));
+        return Math.min(minCost3(cost, n - 1, memo), minCost3(cost, n - 2, memo));
     }
 
     // recursively calculate cost to reach i:th step
-    public int minCost2(int[] cost, int index, int[] memo) {
+    public int minCost3(int[] cost, int index, int[] memo) {
         if (index == 0) return cost[0]; // cost of first step
         if (index == 1) return cost[1]; // cost of second step
 
@@ -52,7 +75,7 @@ public class LC_746_MinCostClimbingStairs {
         if (memo[index] != -1) return memo[index];
 
         // calculate and memoize cost of reaching current step (cost of current step + the MIN cost of reaching the previous two steps)
-        memo[index] = cost[index] + Math.min(minCost2(cost, index - 1, memo), minCost2(cost, index - 2, memo));
+        memo[index] = cost[index] + Math.min(minCost3(cost, index - 1, memo), minCost3(cost, index - 2, memo));
 
         return memo[index];
     }
@@ -60,19 +83,19 @@ public class LC_746_MinCostClimbingStairs {
     /* -------------------------------------------------------------------------------------------- */
 
     // brute force solution, highly inefficient in both space and time
-    public int minCostClimbingStairs3(int[] cost) {
+    public int minCostClimbingStairs4(int[] cost) {
         int n = cost.length; // also the "index" of the "step" we need to reach
 
         // start recursion to calculate cost of reaching the top ("index" n)
-        return Math.min(minCost3(cost, n - 1), minCost3(cost, n - 2));
+        return Math.min(minCost4(cost, n - 1), minCost4(cost, n - 2));
     }
 
     // recursively calculate cost to reach i:th step
-    public int minCost3(int[] cost, int index) {
+    public int minCost4(int[] cost, int index) {
         if (index == 0) return cost[0]; // cost of first step
         if (index == 1) return cost[1]; // cost of second step
 
         // cost for i:th step == cost of current step + the MIN cost of reaching the previous two steps
-        return cost[index] + Math.min(minCost3(cost, index - 1), minCost3(cost, index - 2));
+        return cost[index] + Math.min(minCost4(cost, index - 1), minCost4(cost, index - 2));
     }
 }
