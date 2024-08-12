4ef0a7ccd2fb3c6419492c0f2e2573973bcef722
Juninger <marcus.juninger@gmail.com>
2024-08-12 13:29:29 +0200

    Update LC_567_PermutationInString.java

--- LC_567_PermutationInString.java
+++ LC_567_PermutationInString.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
  * In other words, return true if one of s1's permutations is the substring of s2.
@@ -14,6 +16,31 @@
  */
 public class LC_567_PermutationInString {
     public boolean checkInclusion(String s1, String s2) {
-        
+        if (s1.length() > s2.length()) return false; // impossible edge case
+
+        HashMap<Character, Integer> s1FreqMap = new HashMap<>(); // character frequencies for string s1
+        HashMap<Character, Integer> s2FreqMap =  new HashMap<>(); // characters in current sliding window of s2
+
+        // build frequency map for s1
+        for (char c : s1.toCharArray()) {
+            s1FreqMap.put(c, s1FreqMap.getOrDefault(c, 0) + 1); // +1 frequency of current char
+        }
+
+        // iteratively move sliding window over s2
+        for (int i = 0; i < s2.length(); i++) {
+            char c1 = s2.charAt(i);
+            s2FreqMap.put(c1, s2FreqMap.getOrDefault(c1, 0) + 1); // +1 frequency of current char
+
+            if (i >= s1.length()) { // keep sliding window same size as length of s1
+                char c2 = s2.charAt(i - s1.length()); // character to be removed from window
+                s2FreqMap.put(c2, s2FreqMap.get(c2) - 1); // -1 frequency of current char
+
+                if (s2FreqMap.get(c2) == 0) s2FreqMap.remove(c2); // if no more occurrences, delete the entry
+            }
+
+            if (s1FreqMap.equals(s2FreqMap)) return true; // compare frequency maps to check for permutations
+        }
+
+        return false; // no permutations
     }
 }
