61323f9b102e2cae2b8c1633337a622823dfb308
Juninger <marcus.juninger@gmail.com>
2024-08-14 14:57:52 +0200

    Update LC_230_KthSmallestElementInABST.java

--- LC_230_KthSmallestElementInABST.java
+++ LC_230_KthSmallestElementInABST.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * Given the root of a binary search tree, and an integer k,
  * return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
@@ -11,10 +13,29 @@
  * Output: 3
  */
 public class LC_230_KthSmallestElementInABST {
-    public int kthSmallest(TreeNode root, int k) {
-        return 0;
-    }
 
+    // uses in-order traversal to process nodes in ascending order
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root; // current node being processed
+        int index = 0; // counter for nodes visited until k
+
+        // traverse all nodes
+        while (!stack.isEmpty() || curr != null) {
+            // traverse the left side to the final node
+            while (curr != null) {
+                stack.push(curr); // keep adding nodes to stack
+                curr = curr.left; // move current pointer
+            }
+
+            curr = stack.pop();
+            index++; // increment number of nodes visited
+            if (index == k) return curr.val; // Kth node reached, solution found
+
+            curr = curr.right; // process right subtree
+        }
+        return -1; // technically not reachable
+    }
 
     // definition for a binary tree node
     public class TreeNode {
@@ -29,5 +50,4 @@ public class LC_230_KthSmallestElementInABST {
             this.right = right;
         }
     }
-
 }
