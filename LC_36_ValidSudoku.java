9e30eb7f86b8b3aaf1cb629b0110d485bcf54fc6
Juninger <marcus.juninger@gmail.com>
2024-08-19 12:21:50 +0200

    Update LC_36_ValidSudoku.java

--- LC_36_ValidSudoku.java
+++ LC_36_ValidSudoku.java
@@ -15,7 +15,48 @@ import java.util.Set;
  * Only the filled cells need to be validated according to the mentioned rules.
  */
 class LC_36_ValidSudoku {
+    // brute-force solution to check rows->cols->subBoxes individually
     public boolean isValidSudoku(char[][] board) {
+
+        // check rows for duplicates
+        for (int row = 0; row < 9; row++) {
+            Set<Character> rowSet = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[row][j] == '.') continue; // empty square
+                if (rowSet.contains(board[row][j])) return false; // duplicate found
+                rowSet.add(board[row][j]);
+            }
+        }
+
+        // check columns for duplicates
+        for (int col = 0; col < 9; col++) {
+            Set<Character> colSet = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[j][col] == '.') continue; // empty square
+                if (colSet.contains(board[j][col])) return false; // duplicate found
+                colSet.add(board[j][col]);
+            }
+        }
+
+        // check 3x3 sub-boxes for duplicates
+        for (int boxRowStart = 0; boxRowStart < 9; boxRowStart += 3) { // iterate starting points of each 3x3 sub-box
+            for (int boxColStart = 0; boxColStart < 9; boxColStart += 3) {
+                Set<Character> subBox = new HashSet<>();
+                for (int i = 0; i < 3; i++) { // rows in sub-box
+                    for (int j = 0; j < 3; j++) { // columns in sub-box
+                        char curr = board[boxRowStart + i][boxColStart + j];
+                        if (curr == '.') continue; // empty square
+                        if (subBox.contains(curr)) return false; // duplicate found
+                        subBox.add(curr);
+                    }
+                }
+            }
+        }
+        return true; // valid sudoku
+    }
+
+    // more optimized solution from NeetCode
+    public boolean isValidSudoku2(char[][] board) {
         Map<Integer, Set<Character>> rowSet = new HashMap<>();
         Map<Integer, Set<Character>> colSet = new HashMap<>();
         Map<Integer, Set<Character>> subBox = new HashMap<>();
