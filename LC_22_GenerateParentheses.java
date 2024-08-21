e1ca817896f0cacb4e5af82cba152bc475e3bb0e
Juninger <marcus.juninger@gmail.com>
2024-08-21 14:58:31 +0200

    Create LC_22_GenerateParentheses.java

---
+++ LC_22_GenerateParentheses.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * Example 1:
+ * Input: n = 3
+ * Output: ["((()))","(()())","(())()","()(())","()()()"]
+ *
+ * Example 2:
+ * Input: n = 1
+ * Output: ["()"]
+ */
+public class LC_22_GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        
+    }
+}
