8f41d427b0da50a4e234630d56596ef5e45b50e3
Juninger <marcus.juninger@gmail.com>
2024-08-21 16:05:11 +0200

    Update LC_22_GenerateParentheses.java

--- LC_22_GenerateParentheses.java
+++ LC_22_GenerateParentheses.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,7 +13,33 @@ import java.util.List;
  * Output: ["()"]
  */
 public class LC_22_GenerateParentheses {
+
+    List<String> result = new ArrayList<>(); // stores finished combinations of well-formed parentheses
+    StringBuilder sb = new StringBuilder(); // used as a "pseudo-stack" to store parentheses
+
+    // driver method to start recursion
     public List<String> generateParenthesis(int n) {
-        
+        backtrack(0, 0, n);
+        return result;
+    }
+
+    // recursive method which tracks conditions for building valid parentheses combinations
+    public void backtrack(int open, int closed, int n) {
+        if (open == n && closed == n) { // finished (all opening and closing parentheses used), StringBuilder contains our proper parentheses
+            result.add(sb.toString());
+            return;
+        }
+
+        // recursive part
+        if (open < n) { // we can add more opening parentheses
+            sb.append("("); // add opening parentheses
+            backtrack(open + 1, closed, n); // backtrack and add +1 to count of opening parentheses
+            sb.deleteCharAt(sb.length() - 1); // clean up "stack" to try other possibilities
+        }
+        if (closed < open) { // we can add more closing parentheses
+            sb.append(")"); // add closing parentheses
+            backtrack(open, closed + 1, n); // backtrack and add +1 to count of closing parentheses
+            sb.deleteCharAt(sb.length() - 1); // clean up "stack" to try other possibilities
+        }
     }
 }
