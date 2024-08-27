94d020551b87521afb57eb54f5067c7176c9aabb
Juninger <marcus.juninger@gmail.com>
2024-08-27 12:58:29 +0200

    Update LC_287_FindTheDuplicateNumber.java

--- LC_287_FindTheDuplicateNumber.java
+++ LC_287_FindTheDuplicateNumber.java
@@ -12,7 +12,31 @@
  * Output: 3
  */
 public class LC_287_FindTheDuplicateNumber {
-    public int findDuplicate(int[] nums) {
 
+    /*
+    --Floyd's Tortoise and Hare algorithm for cycle detection in linked lists--
+    - We imagine the input array as a linked list where every element points to the next INDEX (e.g. nums[i] would be the next node to visit)
+    - Since we have n+1 numbers that are all between 1 and 'n', there must be a "cycle" in our imaginary linked list (two nodes will point to the same NEXT-node)
+    - By using two pointers, we can find where the "cycle" starts which will also be our duplicate number
+    */
+    public int findDuplicate(int[] nums) {
+        // initialize pointers to first element of the array
+        int tortoise = nums[0]; // slow pointer
+        int hare = nums[0]; // fast pointer
+
+        // first we need both pointers to be placed somewhere within the cycle
+        do {
+            tortoise = nums[tortoise]; // move ONE step at a time with the slow pointer
+            hare = nums[nums[hare]]; // move TWO steps at a time with the fast pointer
+        } while (tortoise != hare); // pointers met
+
+        tortoise = nums[0]; // reset slow pointer (keep fast pointer at meeting point from previous loop)
+
+        // now we try to find the duplicate number by checking the NEXT-nodes of both the fast and slow pointers
+        while (tortoise != hare) { // this time we move both pointers ONE step at a time until they meet
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        return tortoise; // duplicate number
     }
 }
